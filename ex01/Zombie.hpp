4a97e8a36fcfb378428c74b87288447c047c1c32
Zasenko <dmitry.zasenko@gmail.com>
2025-06-26 11:35:08 +0200

    start ex01 homework

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,18 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie {
+    public:
+        void announce(void);
+        Zombie(std::string name);
+        ~Zombie();
+    private:
+        std::string name;
+};
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+#endif
